3f54b426ad1be7477bacf52e0939a1f4db57a3db
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-11-30 17:37:15 -0500

    Make it work

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log("Hello World");
+var fs = require("fs");
+var program = require("commander");
+
+program
+    .version("0.0.0")
+    .usage("<file>")
+    .parse(process.argv);
+
+program.args.forEach(parse);
+
+function parse(path) {
+    var str = fs.readFileSync(path).toString();
+    eval("var js = " + str);
+    var json = JSON.stringify(js, null, 2);
+    fs.writeFileSync(path, json);
+}
