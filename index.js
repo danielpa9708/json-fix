3f8f281c3ea86bbda6259f43ca02d4ca7a4c079f
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-12-07 13:14:33 -0500

    Add `--no-sort` option (sort by default)

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var fs = require("fs");
 var jsonic = require("jsonic");
 var program = require("commander");
+var stringify = require("json-stable-stringify");
 var packagejson = require("./package.json");
 
 program
     .version(packagejson.version)
-    .option('-s, --spaces [amount]', 'Number of spaces for indentation')
+    .option("-s, --spaces [amount]", "Number of spaces for indentation")
+    .option("--no-sort", "Don't sort")
     .usage("<file>")
     .parse(process.argv);
 
@@ -16,12 +18,18 @@ if (spaces === undefined) {
     spaces = "4";
 }
 var spaces = parseInt(spaces);
+var sort = program.sort;
 
 program.args.forEach(parse);
 
 function parse(path) {
     var str = fs.readFileSync(path).toString();
     var js = jsonic(str);
-    var json = JSON.stringify(js, null, spaces) + "\n";
+    var json = "";
+    if (sort) {
+        json = stringify(js, { space: spaces });
+    } else {
+        json = JSON.stringify(js, null, spaces) + "\n";
+    }
     fs.writeFileSync(path, json);
 }
