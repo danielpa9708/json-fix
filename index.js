2cfd30274f17f23b4aef667109b3bb2278c7d2ad
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-07-01 01:28:42 -0400

    style: use 2-space indentation

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ var inflect = require("inflect-json");
 var allowedInflection = ["none", "pascalcase"];
 
 program
-    .version(packagejson.version)
-    .option("-s, --spaces <amount>", "Number of spaces for indentation", parseInt)
-    .option("-i, --inflection <none|pascalcase>", "Inflection type, defaults to none")
-    .option("--no-sort", "Don't sort")
-    .usage("[options] [file ...]")
-    .parse(process.argv);
+  .version(packagejson.version)
+  .option("-s, --spaces <amount>", "Number of spaces for indentation", parseInt)
+  .option("-i, --inflection <none|pascalcase>", "Inflection type, defaults to none")
+  .option("--no-sort", "Don't sort")
+  .usage("[options] [file ...]")
+  .parse(process.argv);
 
 var spaces = program.spaces;
 if (spaces === undefined) {
-    spaces = 4;
+  spaces = 4;
 }
 var spaces = spaces;
 var sort = program.sort;
@@ -29,33 +29,33 @@ var inflection = program.inflection || allowedInflection[0];
 assert([allowedInflection].indexOf(inflection), `Inflection ${inflection} not allowed`);
 
 if (program.args.length) {
-    program.args.forEach(parse);
+  program.args.forEach(parse);
 } else {
-    getStdin()
-        .then((src) => {
-            const result = parseString(src);
-            process.stdout.write(result);
-        })
-        .catch((err) => {
-            console.err(err);
-            process.exit(1);
-        });
+  getStdin()
+    .then((src) => {
+      const result = parseString(src);
+      process.stdout.write(result);
+    })
+    .catch((err) => {
+      console.err(err);
+      process.exit(1);
+    });
 }
 
 function parse(path) {
-    var str = fs.readFileSync(path).toString();
-    const json = parseString(str);
-    fs.writeFileSync(path, json);
+  var str = fs.readFileSync(path).toString();
+  const json = parseString(str);
+  fs.writeFileSync(path, json);
 }
 
 function parseString(str) {
-    var js = jsonic(str);
-    if (inflection === "pascalcase") {
-        js = inflect.pascalcase(js);
-    }
-    if (sort) {
-        return sortJson(js, { space: spaces }) + "\n";
-    } else {
-        return JSON.stringify(js, null, spaces) + "\n";
-    }
+  var js = jsonic(str);
+  if (inflection === "pascalcase") {
+    js = inflect.pascalcase(js);
+  }
+  if (sort) {
+    return sortJson(js, { space: spaces }) + "\n";
+  } else {
+    return JSON.stringify(js, null, spaces) + "\n";
+  }
 }
--- test/index.js
+++ test/index.js
@@ -7,47 +7,47 @@ const util = require("util");
 const get = util.promisify(cmd.get);
 
 describe("json-fix", function() {
-    const indexsrc = path.resolve(__dirname, "../index.js");
-    beforeEach(async function() {
-        await fs.ensureDir(path.join(__dirname, "fixture"));
-    });
-    afterEach(async function() {
-        await fs.remove(path.join(__dirname, "fixture"));
-    });
-    it("should work with exapmle", async function() {
-        const testsrc = path.join(__dirname, "fixture/test.json");
-        const testjson = "{a:3,b:2}";
-        const command = `node ${indexsrc} ${testsrc}`;
-        const expected = `{
+  const indexsrc = path.resolve(__dirname, "../index.js");
+  beforeEach(async function() {
+    await fs.ensureDir(path.join(__dirname, "fixture"));
+  });
+  afterEach(async function() {
+    await fs.remove(path.join(__dirname, "fixture"));
+  });
+  it("should work with exapmle", async function() {
+    const testsrc = path.join(__dirname, "fixture/test.json");
+    const testjson = "{a:3,b:2}";
+    const command = `node ${indexsrc} ${testsrc}`;
+    const expected = `{
   "a": 3,
   "b": 2
 }
 `;
-        await fs.writeFile(testsrc, testjson);
-        await get(command);
-        const result = await fs.readFile(testsrc);
-        expect(result.toString()).to.be.eq(expected);
-    });
-    it("should work from stdin", async function() {
-        const testjson = "{a:3,b:2}";
-        const command = `echo ${testjson} | node ${indexsrc}`;
-        const expected = `{
+    await fs.writeFile(testsrc, testjson);
+    await get(command);
+    const result = await fs.readFile(testsrc);
+    expect(result.toString()).to.be.eq(expected);
+  });
+  it("should work from stdin", async function() {
+    const testjson = "{a:3,b:2}";
+    const command = `echo ${testjson} | node ${indexsrc}`;
+    const expected = `{
   "a": 3,
   "b": 2
 }
 `;
-        const result = await get(command);
-        expect(result.toString()).to.be.eq(expected);
-    });
-    it("should work with inflection=pascalcase", async function() {
-        const testsrc = path.join(__dirname, "fixture/test.json");
-        await fs.writeFile(testsrc, "{a:3,b:2}");
-        await get(`node ${indexsrc} ${testsrc} --inflection pascalcase`);
-        const result = await fs.readFile(testsrc);
-        expect(result.toString()).to.be.eq(`{
+    const result = await get(command);
+    expect(result.toString()).to.be.eq(expected);
+  });
+  it("should work with inflection=pascalcase", async function() {
+    const testsrc = path.join(__dirname, "fixture/test.json");
+    await fs.writeFile(testsrc, "{a:3,b:2}");
+    await get(`node ${indexsrc} ${testsrc} --inflection pascalcase`);
+    const result = await fs.readFile(testsrc);
+    expect(result.toString()).to.be.eq(`{
   "A": 3,
   "B": 2
 }
 `);
-    });
+  });
 });
