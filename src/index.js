bf75ea29ea23b391df9b921c5bc92fbf4ac47b69
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-07-19 13:43:04 -0400

    refactor: add lynt

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-var assert = require("assert");
-var fs = require("fs");
-var getStdin = require("get-stdin");
-var jsonic = require("jsonic");
-var program = require("commander");
-var sortJson = require("good-sort-json");
-var packagejson = require("../package.json");
-var inflect = require("inflect-json");
+const assert = require("assert");
+const fs = require("fs");
+const getStdin = require("get-stdin");
+const jsonic = require("jsonic");
+const program = require("commander");
+const sortJson = require("good-sort-json");
+const packagejson = require("../package.json");
+const inflect = require("inflect-json");
 
-var allowedInflection = ["none", "pascalcase"];
+const allowedInflection = ["none", "pascalcase"];
 
 program
   .version(packagejson.version)
@@ -19,13 +19,12 @@ program
   .usage("[options] [file ...]")
   .parse(process.argv);
 
-var spaces = program.spaces;
+let spaces = program.spaces;
 if (spaces === undefined) {
   spaces = 4;
 }
-var spaces = spaces;
-var sort = program.sort;
-var inflection = program.inflection || allowedInflection[0];
+const sort = program.sort;
+const inflection = program.inflection || allowedInflection[0];
 assert([allowedInflection].indexOf(inflection), `Inflection ${inflection} not allowed`);
 
 if (program.args.length) {
@@ -43,13 +42,13 @@ if (program.args.length) {
 }
 
 function parse(path) {
-  var str = fs.readFileSync(path).toString();
+  const str = fs.readFileSync(path).toString();
   const json = parseString(str);
   fs.writeFileSync(path, json);
 }
 
 function parseString(str) {
-  var js = jsonic(str);
+  let js = jsonic(str);
   if (inflection === "pascalcase") {
     js = inflect.pascalcase(js);
   }
